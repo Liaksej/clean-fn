c7dc0e28f07130e30d6f0fdcd18aae6b32d4b616
Liaksej <tarusov.liaksej@gmail.com>
2023-05-09 14:30:11 +0200

    Initial commit

---
+++ src/health.js
@@ -0,0 +1,10 @@
+export default function healthStatus(personObject) {
+  if (personObject.health > 50) {
+    return "healthy";
+  }
+  if (personObject.health <= 50 && personObject.health >= 15) {
+    return "wounded";
+  } else {
+    return "critical";
+  }
+}
